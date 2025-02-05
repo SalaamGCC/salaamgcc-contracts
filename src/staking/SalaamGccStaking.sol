fa63c1caa3c21a0a9aadd13f8100efb7244d1825
grsLammy <grspro98@gmail.com>
2025-02-05 15:46:07 +0530

    feat(src): merge helper functions in staking contract

--- src/staking/SalaamGccStaking.sol
+++ src/staking/SalaamGccStaking.sol
@@ -2,15 +2,19 @@
 pragma solidity 0.8.28;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { HelperFunctions } from "../utils/HelperFunctions.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title SalaamGCC Staking Contract
 /// @author SalaamGCC
 /// @notice Handles staking of given token and distribute Rewards tokens
-contract SalaamGccStaking is HelperFunctions {
+contract SalaamGccStaking is Ownable(msg.sender), ReentrancyGuard {
     /// @notice Thrown when an invalid (zero) amount is provided
     error ZeroAmountNotAllowed();
+
+    /// @notice Thrown when caller does not have right access
+    error CallerDoesNotHaveAccess();
 
     /// @notice Thrown when staking is not allowed
     error StakingNotAllowed();
@@ -39,6 +43,7 @@ contract SalaamGccStaking is HelperFunctions {
     IERC20 public stakingToken;
 
     bool public isWithdrawEnable;
+    uint256 public stoppedAt;
     uint256 public rewardRate;
     uint256 public rewardPool;
     uint256 public stakingCap;
@@ -52,6 +57,7 @@ contract SalaamGccStaking is HelperFunctions {
 
     mapping(address user => uint256 rewards) public rewards;
     mapping(address user => uint256 balanceOf) public balanceOf;
+    mapping(address _user => bool sweepers) public sweepers;
 
     /// @notice Emitted when the reward is added
     /// @param reward the amount of reward added
@@ -85,6 +91,23 @@ contract SalaamGccStaking is HelperFunctions {
     /// @param token the address of recovered token
     /// @param amount the amount of recovered token
     event Recovered(address token, uint256 amount);
+
+    /// @notice Emitted when a token is sweeped
+    /// @param token the address of the token sweeped
+    /// @param amount the amount of the token sweeped
+    event Sweeped(address indexed token, uint256 amount);
+
+    /// @notice Emitted when sweeper address is set
+    /// @param account the address of the sweeper
+    /// @param enable the status of sweeper
+    event SetSweeper(address account, bool enable);
+
+    /// @notice Emitted when staking is stop
+    /// @param at the unix time when staking stops
+    event StakingStopped(uint256 at);
+
+    /// @notice Emitted when has started
+    event StakingStart();
 
     /// @dev Constructor that initializes the contract with the given parameters
     /// @param _rewardsToken The address of the rewards token
@@ -262,5 +285,41 @@ contract SalaamGccStaking is HelperFunctions {
         if (_amount == 0) revert ZeroAmountNotAllowed();
         IERC20(_token).safeTransfer(msg.sender, _amount);
         emit Recovered(_token, _amount);
+    }
+
+    /// @notice Sets or unsets a sweeper
+    /// @dev Can only be called by the owner
+    /// @param account The address of the account to set or unset as a sweeper
+    /// @param enable A boolean indicating if the account should be enabled as a sweeper
+    function setSweeper(address account, bool enable) external onlyOwner {
+        sweepers[account] = enable;
+        emit SetSweeper(account, enable);
+    }
+
+    /// @notice Sweeps tokens to the caller
+    /// @dev Caller must be an owner or a sweeper
+    /// @param token The address of the token to sweep
+    /// @param amount The amount of tokens to sweep
+    function sweep(address token, uint256 amount) external {
+        if (!(sweepers[msg.sender] || msg.sender == owner())) {
+            revert CallerDoesNotHaveAccess();
+        }
+
+        IERC20(token).transfer(msg.sender, amount);
+        emit Sweeped(token, amount);
+    }
+
+    /// @notice Stops the staking process
+    /// @dev Can only be called by the owner
+    function stopStaking() external onlyOwner {
+        stoppedAt = block.timestamp;
+        emit StakingStopped(block.timestamp);
+    }
+
+    /// @notice Start the staking process
+    /// @dev Can only be called by the owner
+    function startStaking() external onlyOwner {
+        stoppedAt = 0;
+        emit StakingStart();
     }
 }
